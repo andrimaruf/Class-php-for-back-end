860c664b5b6e6aa9d3847eb152271edb4225532a
Andri Ma'ruf Anshori <and_drie69@yahoo.com>
2014-05-10 11:57:33 +0700

    Create config.init.php

---
+++ lib/config.init.php
@@ -0,0 +1,7 @@
+<?php if ( !defined('BASEPATH') ) exit('No direct script access allowed');
+
+define('DB_NAME', 'susu');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '');
+define('DB_HOST', 'localhost');
+
